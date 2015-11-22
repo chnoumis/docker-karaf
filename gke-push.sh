07067de6842200a3bc4330ccfce37f621ef16ac4
rajan <vrajan@chnoumis.com>
2015-11-22 19:35:53 +1100

    Add new STI

--- gke-push.sh
+++ gke-push.sh
@@ -2,7 +2,6 @@
 
 # Google Container Registry
 PROJECT_ID=$(curl -s 'http://metadata/computeMetadata/v1/project/project-id' -H 'Metadata-Flavor: Google')
-erb Dockerfile.erb > Dockerfile
 docker build -t ${PROJECT_NAME}:${PROJECT_VERSION} .
 docker tag -f ${PROJECT_NAME}:${PROJECT_VERSION} asia.gcr.io/${PROJECT_ID}/${PROJECT_NAME}:${PROJECT_VERSION}
 gcloud docker push asia.gcr.io/${PROJECT_ID}/${PROJECT_NAME}:${PROJECT_VERSION}
